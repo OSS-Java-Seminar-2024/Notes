fade47ada4f81d6b389608093d85a8edc6bbf1cc
Boris <coricboris7@gmail.com>
2024-11-11 10:32:31 +0100

    Create SpringBoot applcation

---
+++ Teatarapp/src/main/java/smb/teatar/test/TeatarappApplication.java
@@ -0,0 +1,13 @@
+package smb.teatar.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatarappApplication.class, args);
+		System.out.println("Welcome to Teatar app!");
+	}
+}
---
+++ Teatarapp/src/test/java/smb/teatar/test/TeatarappApplicationTests.java
@@ -0,0 +1,13 @@
+package smb.teatar.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
